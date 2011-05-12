835c79b623a438488f1befea841a52a31ad8942a
Silvio Hermann <Possommi@users.noreply.github.com>
2011-05-12 12:15:07 +0000

    fixed bug #3300984
    git-svn-id: file:///Users/thosch/test/mycore-svn/svn/maven/mets-model/trunk@20871 7089d063-bc14-496d-ac90-53de1872eb6c

--- src/main/java/org/mycore/mets/model/struct/LogicalDiv.java
+++ src/main/java/org/mycore/mets/model/struct/LogicalDiv.java
@@ -3,13 +3,12 @@ package org.mycore.mets.model.struct;
 import org.jdom.Element;
 
 /**
- * 
- *
  * @author Matthias Eichner
  */
 public class LogicalDiv extends AbstractLogicalDiv {
-    
-    public final static String XML_AMDID = "AMDID";
+
+    public final static String XML_AMDID = "ADMID";
+
     public final static String XML_DMDID = "DMDID";
 
     protected String dmdId, amdId;
@@ -17,6 +16,7 @@ public class LogicalDiv extends AbstractLogicalDiv {
     public LogicalDiv(String id, String type, String label) {
         this(id, type, label, -1, null, null);
     }
+
     public LogicalDiv(String id, String type, String label, int order) {
         this(id, type, label, order, null, null);
     }
@@ -30,12 +30,15 @@ public class LogicalDiv extends AbstractLogicalDiv {
     public void setAmdId(String amdId) {
         this.amdId = amdId;
     }
+
     public void setDmdId(String dmdId) {
         this.dmdId = dmdId;
     }
+
     public String getAmdId() {
         return amdId;
     }
+
     public String getDmdId() {
         return dmdId;
     }
@@ -43,10 +46,10 @@ public class LogicalDiv extends AbstractLogicalDiv {
     @Override
     public Element asElement() {
         Element div = super.asElement();
-        if(this.getAmdId() != null && !this.getAmdId().equals("")) {
+        if (this.getAmdId() != null && !this.getAmdId().equals("")) {
             div.setAttribute(XML_AMDID, this.getAmdId());
         }
-        if(this.getDmdId() != null && !this.getDmdId().equals("")) {
+        if (this.getDmdId() != null && !this.getDmdId().equals("")) {
             div.setAttribute(XML_DMDID, this.getDmdId());
         }
         return div;
