725d910aede54aeaffc8855ca67378c18486dd97
Silvio Hermann <Possommi@users.noreply.github.com>
2011-05-17 07:56:07 +0000

    code format, added java doc
    git-svn-id: file:///Users/thosch/test/mycore-svn/svn/maven/mets-model/trunk@20917 7089d063-bc14-496d-ac90-53de1872eb6c

--- src/main/java/org/mycore/mets/model/IMetsElement.java
+++ src/main/java/org/mycore/mets/model/IMetsElement.java
@@ -1,20 +1,20 @@
-/* $Revision$ 
- * $Date$ 
- * $LastChangedBy$
- * Copyright 2010 - Thüringer Universitäts- und Landesbibliothek Jena
- *  
- * Mets-Editor is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Mets-Editor is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Mets-Editor.  If not, see http://www.gnu.org/licenses/.
+/*
+ * $Revision$ $Date$
+ * $LastChangedBy$ Copyright 2010 - Thüringer Universitäts- und
+ * Landesbibliothek Jena
+ * 
+ * Mets-Editor is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Mets-Editor is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * Mets-Editor. If not, see http://www.gnu.org/licenses/.
  */
 package org.mycore.mets.model;
 
@@ -23,17 +23,22 @@ import org.jdom.Namespace;
 
 /**
  * @author Silvio Hermann (shermann)
- * 
  */
 public interface IMetsElement {
-    public static final Namespace METS = Namespace.getNamespace("mets", "http://www.loc.gov/METS/");
-    public static final Namespace XLINK = Namespace.getNamespace("xlink",
-            "http://www.w3.org/1999/xlink");
-    public static final Namespace MODS = Namespace.getNamespace("mods",
-            "http://www.loc.gov/mods/v3");
 
+    /** Namespace constant for METS namespace */
+    public static final Namespace METS = Namespace.getNamespace("mets", "http://www.loc.gov/METS/");
+
+    /** Namespace constant for XLINK namespace */
+    public static final Namespace XLINK = Namespace.getNamespace("xlink", "http://www.w3.org/1999/xlink");
+
+    /** Namespace constant for MODS namespace */
+    public static final Namespace MODS = Namespace.getNamespace("mods", "http://www.loc.gov/mods/v3");
+
+    /** Namespace constant for XSI namespace */
     public static final Namespace XSI = Namespace.getNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
-    
+
+    /** Namespace constant for DV namespace */
     public static final Namespace DV = Namespace.getNamespace("dv", "http://dfg-viewer.de/");
 
     /**
