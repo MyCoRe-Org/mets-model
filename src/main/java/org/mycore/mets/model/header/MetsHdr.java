e2ba27d650b4f34106b2def364abb385906e1e9b
M3ssman <uwe.hartwig@bitsrc.info>
2020-10-10 18:02:48 +0200

    [app][feat] populate metsHdr section

--- src/main/java/org/mycore/mets/model/header/MetsHdr.java
+++ src/main/java/org/mycore/mets/model/header/MetsHdr.java
@@ -1,169 +1,212 @@
 package org.mycore.mets.model.header;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoField;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jdom2.Element;
 import org.mycore.mets.model.IMetsElement;
 
 /**
- * The mets header element &lt;metsHdr&gt; captures metadata about the METS document itself,
- * not the digital object the METS document encodes. Although it records a more
- * limited set of metadata, it is very similar in function and purpose to the headers
- * employed in other schema such as the Text Encoding Initiative (TEI) or in the Encoded
- * Archival Description (EAD).
+ * The mets header element &lt;metsHdr&gt; captures metadata about the METS
+ * document itself, not the digital object the METS document encodes. Although
+ * it records a more limited set of metadata, it is very similar in function and
+ * purpose to the headers employed in other schema such as the Text Encoding
+ * Initiative (TEI) or in the Encoded Archival Description (EAD).
  * 
- * @see <a href="http://www.loc.gov/standards/mets/docs/mets.v1-9.html#metsHdr">metsHdr</a>
+ * @see <a href=
+ *      "http://www.loc.gov/standards/mets/docs/mets.v1-9.html#metsHdr">metsHdr</a>
  * @author Matthias Eichner
  */
 public class MetsHdr implements IMetsElement {
 
-    private String id;
+	private String id;
 
-    private String admId;
+	private String admId;
 
-    private Instant createDate;
+	private LocalDateTime createDate;
 
-    private Instant lastModDate;
+	private LocalDateTime lastModDate;
 
-    private String recordStatus;
+	private String recordStatus;
 
-    /**
-     * Creates a new &lt;metsHdr&gt; object.
-     * 
-     */
-    public MetsHdr() {
-        this.id = null;
-        this.admId = null;
-        this.recordStatus = null;
-        this.setCreateDate(Instant.now());
-        this.setLastModDate(Instant.now());
-    }
+	private List<Agent> agents;
 
-    @Override
-    public Element asElement() {
-        Element metsHdr = new Element("metsHdr", IMetsElement.METS);
-        if (this.id != null) {
-            metsHdr.setAttribute("ID", this.id);
-        }
-        if (this.admId != null) {
-            metsHdr.setAttribute("ADMID", this.admId);
-        }
-        if (this.createDate != null) {
-            metsHdr.setAttribute("CREATEDATE", DateTimeFormatter.ISO_INSTANT.format(this.createDate));
-        }
-        if (this.lastModDate != null) {
-            metsHdr.setAttribute("LASTMODDATE", DateTimeFormatter.ISO_INSTANT.format(this.lastModDate));
-        }
-        if (this.recordStatus != null) {
-            metsHdr.setAttribute("RECORDSTATUS", this.recordStatus);
-        }
-        return metsHdr;
-    }
+	private List<AltRecordID> altRecordIds;
 
-    /**
-     * This attribute uniquely identifies the element within the METS document,
-     * and would allow the element to be referenced unambiguously from another element or
-     * document via an IDREF or an XPTR. For more information on using ID attributes for
-     * internal and external linking see Chapter 4 of the METS Primer.
-     * 
-     * @return the id
-     */
-    public String getId() {
-        return id;
-    }
+	public static final DateTimeFormatter DT_FORMATTER = new DateTimeFormatterBuilder()
+			.appendPattern("uuuu-MM-dd")
+			.appendLiteral('T')
+			.appendPattern("HH:mm:ss")
+			.toFormatter();
 
-    /**
-     * This attribute uniquely identifies the element within the METS document,
-     * and would allow the element to be referenced unambiguously from another element or
-     * document via an IDREF or an XPTR. For more information on using ID attributes for
-     * internal and external linking see Chapter 4 of the METS Primer.
-     * 
-     * @param id the new id
-     */
-    public void setId(String id) {
-        this.id = id;
-    }
+	/**
+	 * Creates a new &lt;metsHdr&gt; object.
+	 * 
+	 */
+	public MetsHdr() {
+		this.id = null;
+		this.admId = null;
+		this.recordStatus = null;
+		this.setCreateDate(LocalDateTime.now());
+		this.setLastModDate(LocalDateTime.now());
+		this.agents = new ArrayList<>();
+		this.altRecordIds = new ArrayList<>();
+	}
 
-    /**
-     * Contains the ID attribute values of the &lt;techMD&gt;, &lt;sourceMD&gt;,
-     * &lt;rightsMD&gt; and/or &lt;digiprovMD&gt; elements within the &lt;amdSec&gt; of the METS document
-     * that contain administrative metadata pertaining to the METS document itself. For
-     * more information on using METS IDREFS and IDREF type attributes for internal
-     * linking, see Chapter 4 of the METS Primer.
-     * 
-     * @return the adm identifier
-     */
-    public String getAdmId() {
-        return admId;
-    }
+	@SuppressWarnings("exports")
+	@Override
+	public Element asElement() {
+		Element metsHdr = new Element("metsHdr", IMetsElement.METS);
+		if (this.id != null) {
+			metsHdr.setAttribute("ID", this.id);
+		}
+		if (this.admId != null) {
+			metsHdr.setAttribute("ADMID", this.admId);
+		}
+		if (this.createDate != null) {
+			String createDate = DT_FORMATTER.format(this.createDate);
+			metsHdr.setAttribute("CREATEDATE", createDate);
+		}
+		if (this.lastModDate != null) {
+			String modDate = DT_FORMATTER.format(this.lastModDate);
+			metsHdr.setAttribute("LASTMODDATE", modDate);
+		}
+		if (this.recordStatus != null) {
+			metsHdr.setAttribute("RECORDSTATUS", this.recordStatus);
+		}
+		if (this.agents != null && !this.agents.isEmpty()) {
+			for (Agent agent : this.agents) {
+				metsHdr.addContent(agent.asElement());
+			}
+		}
+		return metsHdr;
+	}
 
-    /**
-     * Contains the ID attribute values of the &lt;techMD&gt;, &lt;sourceMD&gt;,
-     * &lt;rightsMD&gt; and/or &lt;digiprovMD&gt; elements within the &lt;amdSec&gt; of the METS document
-     * that contain administrative metadata pertaining to the METS document itself. For
-     * more information on using METS IDREFS and IDREF type attributes for internal
-     * linking, see Chapter 4 of the METS Primer.
-     * 
-     * @param admId the new adm identifier
-     */
-    public void setAdmId(String admId) {
-        this.admId = admId;
-    }
+	/**
+	 * This attribute uniquely identifies the element within the METS document, and
+	 * would allow the element to be referenced unambiguously from another element
+	 * or document via an IDREF or an XPTR. For more information on using ID
+	 * attributes for internal and external linking see Chapter 4 of the METS
+	 * Primer.
+	 * 
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
 
-    /**
-     * Records the date/time the METS document was created.
-     * 
-     * @return mets document creation date time
-     */
-    public Instant getCreateDate() {
-        return createDate;
-    }
+	/**
+	 * This attribute uniquely identifies the element within the METS document, and
+	 * would allow the element to be referenced unambiguously from another element
+	 * or document via an IDREF or an XPTR. For more information on using ID
+	 * attributes for internal and external linking see Chapter 4 of the METS
+	 * Primer.
+	 * 
+	 * @param id the new id
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    /**
-     * Records the date/time the METS document was created.
-     * 
-     * @param createDate new creation date
-     */
-    public void setCreateDate(Instant createDate) {
-        this.createDate = Instant.ofEpochMilli(createDate.toEpochMilli()).with(ChronoField.MILLI_OF_SECOND, 0);
-    }
+	/**
+	 * Contains the ID attribute values of the &lt;techMD&gt;, &lt;sourceMD&gt;,
+	 * &lt;rightsMD&gt; and/or &lt;digiprovMD&gt; elements within the &lt;amdSec&gt;
+	 * of the METS document that contain administrative metadata pertaining to the
+	 * METS document itself. For more information on using METS IDREFS and IDREF
+	 * type attributes for internal linking, see Chapter 4 of the METS Primer.
+	 * 
+	 * @return the adm identifier
+	 */
+	public String getAdmId() {
+		return admId;
+	}
 
-    /**
-     * Is used to indicate the date/time the METS document was last modified.
-     * 
-     * @return mets document modified date
-     */
-    public Instant getLastModDate() {
-        return lastModDate;
-    }
+	/**
+	 * Contains the ID attribute values of the &lt;techMD&gt;, &lt;sourceMD&gt;,
+	 * &lt;rightsMD&gt; and/or &lt;digiprovMD&gt; elements within the &lt;amdSec&gt;
+	 * of the METS document that contain administrative metadata pertaining to the
+	 * METS document itself. For more information on using METS IDREFS and IDREF
+	 * type attributes for internal linking, see Chapter 4 of the METS Primer.
+	 * 
+	 * @param admId the new adm identifier
+	 */
+	public void setAdmId(String admId) {
+		this.admId = admId;
+	}
 
-    /**
-     * Is used to indicate the date/time the METS document was last modified.
-     * 
-     * @param lastModDate new modified date
-     */
-    public void setLastModDate(Instant lastModDate) {
-        this.lastModDate = Instant.ofEpochMilli(lastModDate.toEpochMilli()).with(ChronoField.MILLI_OF_SECOND, 0);
-    }
+	/**
+	 * Records the date/time the METS document was created.
+	 * 
+	 * @return mets document creation date time
+	 */
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
 
-    /**
-     * Specifies the status of the METS document. It is used for internal processing purposes.
-     * 
-     * @return status of the mets document
-     */
-    public String getRecordStatus() {
-        return recordStatus;
-    }
+	/**
+	 * Records the date/time the METS document was created.
+	 * 
+	 * @param createDate new creation date
+	 */
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
 
-    /**
-     * Specifies the status of the METS document. It is used for internal processing purposes.
-     * 
-     * @param recordStatus status of this mets document
-     */
-    public void setRecordStatus(String recordStatus) {
-        this.recordStatus = recordStatus;
-    }
+	/**
+	 * Is used to indicate the date/time the METS document was last modified.
+	 * 
+	 * @return mets document modified date
+	 */
+	public LocalDateTime getLastModDate() {
+		return lastModDate;
+	}
+
+	/**
+	 * Is used to indicate the date/time the METS document was last modified.
+	 * 
+	 * @param lastModDate new modified date
+	 */
+	public void setLastModDate(LocalDateTime lastModDate) {
+		this.lastModDate = lastModDate;
+	}
+
+	/**
+	 * Specifies the status of the METS document. It is used for internal processing
+	 * purposes.
+	 * 
+	 * @return status of the mets document
+	 */
+	public String getRecordStatus() {
+		return recordStatus;
+	}
+
+	/**
+	 * Specifies the status of the METS document. It is used for internal processing
+	 * purposes.
+	 * 
+	 * @param recordStatus status of this mets document
+	 */
+	public void setRecordStatus(String recordStatus) {
+		this.recordStatus = recordStatus;
+	}
+
+	public List<Agent> getAgents() {
+		return agents;
+	}
+
+	public void setAgents(List<Agent> agents) {
+		this.agents = agents;
+	}
+
+	public List<AltRecordID> getAltRecordIds() {
+		return altRecordIds;
+	}
+
+	public void setAltRecordIds(List<AltRecordID> altRecordIds) {
+		this.altRecordIds = altRecordIds;
+	}
 
 }
---
+++ src/test/java/org/mycore/mets/model/MetsProcessResourceTest.java
@@ -0,0 +1,136 @@
+package org.mycore.mets.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.mycore.mets.model.header.Agent;
+import org.mycore.mets.model.header.MetsHdr;
+import org.mycore.mets.model.header.Name;
+import org.mycore.mets.model.header.Note;
+
+/**
+ * 
+ * @author Uwe Hartwig (M3ssman)
+ *
+ */
+public class MetsProcessResourceTest {
+
+	@ClassRule
+	@SuppressWarnings("exports")
+	public static TemporaryFolder tempDir = new TemporaryFolder();
+
+	static String sourcePath = "src/test/resources/urn:nbn:de:gbv:3:3-21437.xml";
+
+	static Path targetPath;
+
+	static Mets mets;
+	
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		
+		targetPath = Path.of(tempDir.getRoot().getAbsolutePath(), "mets.xml");
+		Path absPath = Path.of(sourcePath).toAbsolutePath(); 
+		Files.copy(absPath, targetPath);
+		mets = extractMets(targetPath.toString());
+	}
+
+	private static Mets extractMets(String targetPath) throws JDOMException, IOException {
+		File f = new File(targetPath);
+		SAXBuilder builder = new SAXBuilder();
+		Document document = builder.build(f);
+		return new Mets(document);
+	}
+
+	@Test
+	public void testMetsDefaultStructureElementsPresent() {
+		assertNotNull(mets);
+		assertNotNull(mets.getAmdSections());
+		assertEquals(1, mets.getAmdSections().length);
+		assertNotNull(mets.getDmdSections());
+		assertEquals(1, mets.getDmdSections().length);
+		assertNotNull(mets.getFileSec());
+		assertEquals(5, mets.getFileSec().getFileGroups().size());
+		assertNotNull(mets.getLogicalStructMap());
+		assertNotNull(mets.getLogicalStructMap().getDivContainer());
+		assertNotNull(mets.getLogicalStructMap().getDivContainer().getChildren());
+		assertEquals(3, mets.getLogicalStructMap().getDivContainer().getChildren().size());
+		assertNotNull(mets.getPhysicalStructMap());
+		assertNotNull(mets.getPhysicalStructMap().getDivContainer());
+		assertNotNull(mets.getPhysicalStructMap().getDivContainer().getChildren());
+		assertEquals(4, mets.getPhysicalStructMap().getDivContainer().getChildren().size());
+		assertNotNull(mets.getStructLink());
+		assertNotNull(mets.getStructLink().getSmLinks());
+		assertEquals(4, mets.getStructLink().getSmLinks().size());
+	}
+
+	@Test
+	public void testOriginalMetsHdrPresent() {
+		assertNotNull(mets.getMetsHdr());
+	}
+
+	@Test
+	public void testOrignalHetsHdrAgentsPresent() {
+		MetsHdr metsHdr = mets.getMetsHdr();
+		assertNotNull(metsHdr.getAgents());
+		assertEquals(4, metsHdr.getAgents().size());
+	}
+
+	@Test
+	public void testOriginalHetsHdrAgentDetails() {
+		MetsHdr metsHdr = mets.getMetsHdr();
+		assertNotNull(metsHdr.getAgents());
+		assertNotNull(metsHdr.getAgents().get(0).getName());
+		assertEquals("vls/2.12.1", metsHdr.getAgents().get(0).getName().getText());
+		assertNotNull(metsHdr.getAgents().get(1).getName());
+		assertEquals("ulbhal-hspe", metsHdr.getAgents().get(1).getName().getText());
+		assertNotNull(metsHdr.getAgents().get(2).getName());
+		assertEquals("digital.bibliothek.uni-halle.de/hd", metsHdr.getAgents().get(2).getName().getText());
+		assertNotNull(metsHdr.getAgents().get(3).getName());
+		assertEquals("vd", metsHdr.getAgents().get(3).getName().getText());
+	}
+
+	@Test
+	public void testAddNewMetsHdrAgent() throws Exception {
+		MetsHdr metsHdr = mets.getMetsHdr();
+		assertEquals(4, metsHdr.getAgents().size());
+
+		// act
+		Agent agent = new Agent("OTHER", new Name("digitalDerivans"));
+		agent.setNotes(List.of(new Note("created for test purposes")));
+		metsHdr.getAgents().add(agent);
+		String now = MetsHdr.DT_FORMATTER.format(LocalDateTime.now());
+		metsHdr.setLastModDate(LocalDateTime.parse(now));
+
+		// assert
+		assertEquals(5, metsHdr.getAgents().size());
+		
+        try (OutputStream metsOut = Files.newOutputStream(targetPath)) {
+            XMLOutputter xout = new XMLOutputter(Format.getPrettyFormat());
+            xout.output(mets.asDocument(), metsOut);
+        } 
+		
+		// re-act: check datetime serialization
+		Mets mets = extractMets(targetPath.toString());
+		assertEquals(5, mets.getMetsHdr().getAgents().size());
+		String serializedLastModifiedDate = MetsHdr.DT_FORMATTER.format(mets.getMetsHdr().getLastModDate());
+		assertEquals(now, serializedLastModifiedDate);
+	}
+
+}
